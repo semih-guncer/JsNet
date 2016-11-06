e0bb3f8197b9e9cf2c693a262c353aa86e9bce46
semih-guncer <semih_guncer@yahoo.com>
2016-11-06 11:58:09 +0200

    Template support used on Demo

--- JsNetLibDemo/ChartJs/Default.aspx.cs
+++ JsNetLibDemo/ChartJs/Default.aspx.cs
@@ -15,38 +15,60 @@ namespace JsNetLibDemo.ChartJs
 
         }
 
-        public string GetExampleCartPie()
+        public string GetExampleChartBar()
         {
-            ConfigPie c = new ConfigPie();
-            c.data.labels.Add("L1");
-            c.data.labels.Add("L2");
-            c.data.labels.Add("L3");
+            string templateFile = Server.MapPath("TemplateBar.json");
+            ConfigBar c = ChartJsNet.LoadConfigBar(templateFile);
 
-            DatasetPie ds = new DatasetPie();
-            ds.label = "Pie";
+            c.data.datasets[0].data.Clear();
+            c.data.datasets[0].data.Add(120);
+            c.data.datasets[0].data.Add(190);
+            c.data.datasets[0].data.Add(30);
+            c.data.datasets[0].data.Add(50);
+            c.data.datasets[0].data.Add(20);
+            c.data.datasets[0].data.Add(30);
 
+            return ChartJsNet.Serialize(c, 0);
+        }
 
-            ds.backgroundColor.Add("rgba(255, 99, 132, 0.2)");
-            ds.backgroundColor.Add("rgba(54, 162, 235, 0.2)");
-            ds.backgroundColor.Add("rgba(255, 206, 86, 0.2)");
-            ds.backgroundColor.Add("rgba(75, 192, 192, 0.2)");
-            ds.backgroundColor.Add("rgba(153, 102, 255, 0.2)");
-            ds.backgroundColor.Add("rgba(255, 159, 64, 0.2)");
+        public string GetExampleChartPie()
+        {
+            string templateFile = Server.MapPath("TemplatePie.json");
+            ConfigPie c = ChartJsNet.LoadConfigPie(templateFile);
 
-            ds.borderColor.Add("rgba(255, 99, 132, 1)");
-            ds.borderColor.Add("rgba(54, 162, 235, 1)");
-            ds.borderColor.Add("rgba(255, 206, 86, 1)");
-            ds.borderColor.Add("rgba(75, 192, 192, 1)");
-            ds.borderColor.Add("rgba(153, 102, 255, 1)");
-            ds.borderColor.Add("rgba(255, 159, 64, 1)");
+            c.data.datasets[0].data.Clear();
+            c.data.datasets[0].data.Add(120);
+            c.data.datasets[0].data.Add(250);
+            c.data.datasets[0].data.Add(70);
 
-            ds.borderWidth = 1;
+            // ConfigPie c = new ConfigPie();
+            // c.data.labels.Add("L1");
+            // c.data.labels.Add("L2");
+            // c.data.labels.Add("L3");
+            // 
+            // DatasetPie ds = new DatasetPie();
+            // ds.label = "Pie";
+            // ds.backgroundColor.Add("rgba(255, 99, 132, 0.2)");
+            // ds.backgroundColor.Add("rgba(54, 162, 235, 0.2)");
+            // ds.backgroundColor.Add("rgba(255, 206, 86, 0.2)");
+            // ds.backgroundColor.Add("rgba(75, 192, 192, 0.2)");
+            // ds.backgroundColor.Add("rgba(153, 102, 255, 0.2)");
+            // ds.backgroundColor.Add("rgba(255, 159, 64, 0.2)");
+            // 
+            // ds.borderColor.Add("rgba(255, 99, 132, 1)");
+            // ds.borderColor.Add("rgba(54, 162, 235, 1)");
+            // ds.borderColor.Add("rgba(255, 206, 86, 1)");
+            // ds.borderColor.Add("rgba(75, 192, 192, 1)");
+            // ds.borderColor.Add("rgba(153, 102, 255, 1)");
+            // ds.borderColor.Add("rgba(255, 159, 64, 1)");
+            // 
+            // ds.borderWidth = 1;
+            // 
+            // ds.data.Add(12.00);
+            // ds.data.Add(25.00);
+            // ds.data.Add(7.00);
+            // c.data.datasets.Add(ds);
 
-            ds.data.Add(12.00);
-            ds.data.Add(25.00);
-            ds.data.Add(7.00);
-            c.data.datasets.Add(ds);
-            
             return ChartJsNet.Serialize(c, 2);
         }
     }
